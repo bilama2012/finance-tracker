010f9572444b45a2d42010a0a745c4a0b5498023
samirfx <samir@fonix.com>
2021-02-06 22:25:01 +0000

    ci all features

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,18 +1,22 @@
 class Stock < ApplicationRecord
-  # has_many :user_stocks
-  # has_many :users, through: :user_stocks
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
 
-  # validates :name, :ticker, presence: true
+  validates :name, :ticker, presence: true
 
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
                                   endpoint: 'https://sandbox.iexapis.com/v1')
-    # begin
-    #   new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
-    # rescue => exception
-    #   return nil
-    # end
+    begin
+      new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+    rescue => exception
+      return nil
+    end
 
-    client.price(ticker_symbol)
   end
+
+  def self.check_db(tickerer_symbol)
+    where(ticker: tickerer_symbol).first
+  end
+
 end
